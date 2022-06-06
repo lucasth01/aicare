953714f2fa9346a73266dc4d62897bdb0f97fc12
Theodorus Lucas <theodoruslucas@gmail.com>
2022-06-06 15:30:26 +0700

    revision firestore

--- config/firestore.js
+++ config/firestore.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require('../service-account-key.json');
+var serviceAccount = require('../service-account-key.json') || process.env.FIREBASE_SECRET
 
 const firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
