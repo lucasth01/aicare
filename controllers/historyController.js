63199028fa6dc6962d9d397b00c9a0631ab4ba57
Theodorus Lucas <theodoruslucas@gmail.com>
2022-06-07 10:12:02 +0700

    test image

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -32,41 +32,37 @@ const getHistory = async (req, res) => {
 const addHistory = async (req, res) => {
     const { label, percentage } = req.body
     const userid = req.user.id
-    const image = req.file
 
-    console.log(image)
-    res.send('test')
+    const id = 'record-' + uuidv1() 
+    const recordRef = db.collection('history').doc(id)
 
-    // const id = 'record-' + uuidv1() 
-    // const recordRef = db.collection('history').doc(id)
+    try {
+        if(!label || !percentage) {
+            throw new Error('Please fill all the fields.')
+        }
 
-    // try {
-    //     if(!label || !percentage) {
-    //         throw new Error('Please fill all the fields.')
-    //     }
+        const newRecord = {
+            id,
+            userid,
+            outputlabel: label,
+            outputpercentage: percentage,
+            date: new Date()
+        }
 
-    //     const newRecord = {
-    //         id,
-    //         userid,
-    //         outputlabel: label,
-    //         outputpercentage: percentage,
-    //         date: new Date()
-    //     }
+        await recordRef.set(newRecord)
 
-    //     await recordRef.set(newRecord)
+        res.json({
+            error: false,
+            message: 'Record successfully added to history.',
+            newRecord
+        })
 
-    //     res.json({
-    //         error: false,
-    //         message: 'Record successfully added to history.',
-    //         newRecord
-    //     })
-
-    // } catch (err) {
-    //     res.status(400).json({
-    //         error: true,
-    //         message: err.message
-    //     })
-    // }
+    } catch (err) {
+        res.status(400).json({
+            error: true,
+            message: err.message
+        })
+    }
 }
 
 module.exports = {
--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -5,16 +5,7 @@ const {
     addHistory
 } = require('../controllers/historyController')
 const { authorize } = require('../middleware/auth')
-const multer = require('multer')
-const MulterGoogleCloudStorage = require('multer-cloud-storage')
 
-const uploadHandler = multer({
-    storage: MulterGoogleCloudStorage.storageEngine(),
-    filename: (req, file, cb) => {
-        cb(null, `image-${Date.now()}`)
-    }
-})
-
-router.route('/').get(authorize, getHistory).post(authorize, uploadHandler.any(), addHistory)
+router.route('/').get(authorize, getHistory).post(authorize, addHistory)
 
 module.exports = router
