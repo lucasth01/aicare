8e755b7d3664925242e980ecc80315744bfb0a32
Theodorus Lucas <theodoruslucas@gmail.com>
2022-06-07 09:22:02 +0700

    test image

--- config/firestore.js
+++ config/firestore.js
@@ -1,12 +1,12 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require('../service-account-key.json')
+// const serviceAccount = require('../service-account-key.json')
 
-const firebase = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 // const firebase = admin.initializeApp({
-//   credential: admin.credential.applicationDefault()
+//   credential: admin.credential.cert(serviceAccount)
 // });
+const firebase = admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
 
 module.exports = firebase
